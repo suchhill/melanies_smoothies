c85428edf8741ae9f9a27abdbbad8cc97b8a46e0
suchhill <67383370+suchhill@users.noreply.github.com>
2025-10-22 11:52:05 -0400

    Update streamlit_app.py
    made pd dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,6 @@ st.write(
   """
 )
 
-#option = st.selectbox(
- #   'What is your favourite fruit',
- #   ('Banana', 'Strawberries', 'Peaches'),
-#)
-
-#st.write("Your favorite fruit is:", option)
-
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your Smoothie will be", name_on_order)
 
@@ -26,7 +19,11 @@ st.write("The name on your Smoothie will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data = my_dataframe, use_container_width=True)
+#st.dataframe(data = my_dataframe, use_container_width=True)
+#st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
