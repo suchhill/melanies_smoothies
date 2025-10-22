257d9e478548845317c2e4d7eb13d9abb1c08954
suchhill <67383370+suchhill@users.noreply.github.com>
2025-10-22 11:45:47 -0400

    Update streamlit_app.py
    error update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.write("The name on your Smoothie will be", name_on_order)
 #session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data = my_dataframe, use_container_width=True)
 st.stop()
 
